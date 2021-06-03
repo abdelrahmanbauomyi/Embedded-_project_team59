90fe77c2acccdf5b0732ab51a528d212cdff5b84
Abdelrahman Bauomyi <abdo01820@gamil.com>
2021-06-03 03:23:06 +0200

    prototype lcd_display

--- main.c
+++ main.c
@@ -5,38 +5,71 @@
 #define green 0x08
 void init(void)
 {
-	SYSCTL_RCGCGPIO_R |=0x20;
-	while(!(SYSCTL_PRGPIO_R&0x20));
-	GPIO_PORTF_LOCK_R=0x4C4F434B;
-	GPIO_PORTF_CR_R =0x1F;
-	GPIO_PORTF_AFSEL_R=0;
-	GPIO_PORTF_PCTL_R=0;
-	GPIO_PORTF_AMSEL_R=0;
-	GPIO_PORTF_DIR_R=0x0E;
-	GPIO_PORTF_DEN_R=0x1F;
-	GPIO_PORTF_PUR_R=0x11;
 
+}
+void delay(int x)
+{
+
+for(int i =0 ; i<x ; i++)
+{
+for( int j=0 ; j<x ; i++)
+{
+}
+}
+}
+void lcd_command( char command )
+{
+	GPIO_PORTA_DATA_R&=0x1F;
+	GPIO_PORTB_DATA_R=command;
+	GPIO_PORTA_DATA_R|=0x80;
+	delay(50);
+	GPIO_PORTA_DATA_R&=0x1F;
+}
+
+void lcd_data( char data ) 
+{
+	GPIO_PORTA_DATA_R&=0x3F;
+	GPIO_PORTA_DATA_R|=0x20;
+	GPIO_PORTB_DATA_R=data;
+	GPIO_PORTA_DATA_R|=0x80;
+	delay(50);
+	GPIO_PORTA_DATA_R&=0x1F;
+}
+void lcd_number_display(int x)
+{
+	
+}
+void lcd_display(int distance)
+{
+	lcd_command(0x30);
+	lcd_command(0x38);
+	lcd_command(0x01);
+	lcd_command(0x0F);
+	lcd_data('d');
+	lcd_command(0x06);
+	lcd_data('i');
+	lcd_command(0x06);
+	lcd_data('s');
+	lcd_command(0x06);
+	lcd_data('t');
+	lcd_command(0x06);
+	lcd_data('a');
+	lcd_command(0x06);
+	lcd_data('n');
+	lcd_command(0x06);
+	lcd_data('c');
+	lcd_command(0x06);
+	lcd_data('e');
+	lcd_command(0x06);
+	lcd_data(':');
+	lcd_command(0x06);
+	lcd_data(distance);
+	lcd_command(0x06);
 }
 int main()
 {
 	init();
-	while(1)
 
-	{
-		if( (GPIO_PORTF_DATA_R&0x11) == 0x10)
-		{
-			GPIO_PORTF_DATA_R=Red;
-		}
-		else if((GPIO_PORTF_DATA_R&0x11)==0x01){
-			GPIO_PORTF_DATA_R=blue;
-		}
-			
-		else if((GPIO_PORTF_DATA_R&0x11)==0x00)
-		{
-			GPIO_PORTF_DATA_R=green;
-		}
-		else
-			GPIO_PORTF_DATA_R=0;
 		
 	}
-}
+
